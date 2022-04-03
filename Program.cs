be14333abc980fe2d9810a0c665f30a9800fc4ee
AbhishekDevlop <101594544+AbhishekDevlop@users.noreply.github.com>
2022-04-03 10:46:08 +0530

    Add files via upload
    thread and sleep

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadDemo threadDemo = new ThreadDemo();
+
+            Thread t1 = new Thread(new ThreadStart(threadDemo.Test));
+            Thread t2 = new Thread(new ThreadStart(threadDemo.Test));
+            Thread t3 = new Thread(new ThreadStart(threadDemo.Test));
+            t1.Name = "First Thread";
+            t2.Name = "Second Thread";
+            t3.Name = "Third Thread";
+
+            t1.Priority = ThreadPriority.Lowest;   //setting priority
+            t2.Priority = ThreadPriority.BelowNormal;
+            t3.Priority = ThreadPriority.Highest;
+
+            t1.Start();
+            t2.Start();
+            t2.Join();
+            t3.Start();
+
+               
+
+        }
+    }
+}
---
+++ ThreadDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public class ThreadDemo
+    {
+        public void Test() 
+        {
+            lock (this)
+            {
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name);
+                }
+            }
+        }
+
+        //public void Test2() 
+        //{
+        //    for(int i = 0; i < 5; i++) 
+        //    {
+        //        Console.WriteLine(i);
+        //        Thread.Sleep(200);
+        //    }
+        //
+        //}
+    }
+}
